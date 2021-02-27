8c140573db10c1b689300e5c44d3d6da315c02ad
makaveli <prakasbudiawan@gmail.com>
2021-02-27 12:36:45 +0700

    update index login

--- application/modules/dashboard/views/index.php
+++ application/modules/dashboard/views/index.php
@@ -1,11 +1,21 @@
 
-    <br>
-    <a href="<?php echo base_url(); ?>dashboard/form_pembentukan" class="btn btn-sm btn-info"> PENGGABUNGAN KECAMATAN</a>
-    <br>
-    <br>
-    <a href="" class="btn btn-sm btn-info"> PENYESUAIAN KECAMATAN</a>
-    <p>a. Perubahan batas wilayah kecamatan</p>
-    <p>b. Perubahan nama kecamatan</p>
-    <p>c. Pemindahan ibu kota kecamatan</p>
-    <p>d. Perubahan nama ibu kota kecamatan</p>
-    <p>e. Perbaikan redaksional nama kecamatan</p>
+    <div class="dash">
+      <div class="dash-index">
+        <!-- <br> -->
+        <a href="<?php echo base_url(); ?>dashboard/form_pembentukan" class="btn btn-sm btn-info"> PENGGABUNGAN KECAMATAN</a>
+        
+      
+      </div>
+      <div class="dash-index-new">
+      <a href="" class="btn btn-sm btn-info" disabled> PENYESUAIAN KECAMATAN</a>
+      <br>
+        <div class="dash-val">
+          <small class="text-danger pl-1"><i> a. Perubahan batas wilayah kecamatan  </i></small><br>
+          <small class="text-danger pl-1"><i> b. Perubahan nama kecamatan  </i></small><br>
+          <small class="text-danger pl-1"><i> c. Pemindahan ibu kota kecamatan  </i></small><br>
+          <small class="text-danger pl-1"><i> d. Perubahan nama ibu kota kecamatan  </i></small><br>
+          <small class="text-danger pl-1"><i> e. Perbaikan redaksional nama kecamatan  </i></small><br>
+        </div>
+      
+      </div>
+    </div>
